c8055a026f4be0350ba4d76b736ba2e0985835b7
sEGuoer <709074535@qq.com>
2023-10-26 14:27:58 +0800

    hello redis(记得启动redis的img映像)

--- sb-01-redis/src/test/java/com/seguoer/Sb01RedisApplicationTests.java
+++ sb-01-redis/src/test/java/com/seguoer/Sb01RedisApplicationTests.java
@@ -1,13 +1,26 @@
 package com.seguoer;
 
+import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Sb01RedisApplicationTests {
+    @Autowired
+    MockMvc mvc;
 
     @Test
-    void contextLoads() {
+    void helloRedis() throws Exception {
+        mvc.perform(get("/hello-redis"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(StringContains.containsString("count=")));
     }
 
 }
