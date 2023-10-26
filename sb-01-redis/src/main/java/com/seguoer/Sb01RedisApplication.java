adfb3467321189f20c005572af04308f6c2d0ddc
sEGuoer <709074535@qq.com>
2023-10-26 14:09:46 +0800

    create redis module

---
+++ sb-01-redis/src/main/java/com/seguoer/Sb01RedisApplication.java
@@ -0,0 +1,13 @@
+package com.seguoer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb01RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb01RedisApplication.class, args);
+    }
+
+}
