67caf24700ef2e9acb924b8a49e4b42e06400b27
sEGuoer <709074535@qq.com>
2023-10-26 15:38:34 +0800

    create actuator module

---
+++ sb-02-actuator/src/main/java/com/seguoer/Sb02ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.seguoer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb02ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb02ActuatorApplication.class, args);
+	}
+
+}
