c8406666889864d1266187652b79b1187b25c44d
sEGuoer <709074535@qq.com>
2023-10-27 16:35:43 +0800

    create test first starter module

---
+++ sb-04-seguoer-useFirstStarter/src/main/java/com/seguoer/sb04seguoerusefirststarter/Sb04SeguoerUseFirstStarterApplication.java
@@ -0,0 +1,13 @@
+package com.seguoer.sb04seguoerusefirststarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb04SeguoerUseFirstStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb04SeguoerUseFirstStarterApplication.class, args);
+    }
+
+}
---
+++ sb-04-seguoer-useFirstStarter/src/test/java/com/seguoer/sb04seguoerusefirststarter/Sb04SeguoerUseFirstStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguoer.sb04seguoerusefirststarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb04SeguoerUseFirstStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
