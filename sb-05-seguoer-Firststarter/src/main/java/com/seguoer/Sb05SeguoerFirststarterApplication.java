160c5057685993369c1f8e4e37ef3161e6f03557
sEGuoer <709074535@qq.com>
2023-10-30 14:39:38 +0800

    create First-spring-boot-starter module 05

---
+++ sb-05-seguoer-Firststarter/src/main/java/com/seguoer/Sb05SeguoerFirststarterApplication.java
@@ -0,0 +1,13 @@
+package com.seguoer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb05SeguoerFirststarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb05SeguoerFirststarterApplication.class, args);
+    }
+
+}
---
+++ sb-05-seguoer-Firststarter/src/test/java/com/seguoer/Sb05SeguoerFirststarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguoer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb05SeguoerFirststarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
